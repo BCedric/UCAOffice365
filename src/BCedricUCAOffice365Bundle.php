d520d0913a7f7b8a9c4e2af4ff8c0ac747d7572e
Cedbois <cedric.bois@uca.fr>
2026-01-20 11:26:54 +0100

    fix configure bundle

--- src/BCedricUCAOffice365Bundle.php
+++ src/BCedricUCAOffice365Bundle.php
@@ -3,7 +3,7 @@
 namespace BCedric\UCAOffice365;
 
 use BCedric\UCAOffice365\Service\UCAOffice365;
-use BCedric\UCAOffice365\Service\GraphApi;
+use BCedric\UCAOffice365\Service\GraphAPI;
 use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
@@ -38,16 +38,35 @@ class BCedricUCAOffice365Bundle extends AbstractBundle
         $container->services()->set(UCAOffice365::class)
             ->public();
 
-        $builder->autowire(UCAOffice365::class)
-            ->setArgument('$url', $config['uca_api']['url'])
-            ->setArgument('$login', $config['uca_api']['login'])
-            ->setArgument('$password', $config['uca_api']['password'])
-        ;
+        if (array_key_exists('uca_api', $config)) {
 
-        $builder->autowire(GraphApi::class)
-            ->setArgument('$clientId', $config['graph_api']['client'])
-            ->setArgument('$tenantId', $config['graph_api']['tenant'])
-            ->setArgument('$clientSecret', $config['graph_api']['client_secret'])
-        ;
+            $builder->autowire(UCAOffice365::class)
+                ->setArgument('$url', $config['uca_api']['url'])
+                ->setArgument('$login', $config['uca_api']['login'])
+                ->setArgument('$password', $config['uca_api']['password'])
+            ;
+        } else {
+            $builder->autowire(UCAOffice365::class)
+                ->setArgument('$url', '')
+                ->setArgument('$login', '')
+                ->setArgument('$password', '')
+            ;
+        }
+
+        $container->services()->set(GraphAPI::class)
+            ->public();
+        if (array_key_exists('graph_api', $config)) {
+            $builder->autowire(GraphAPI::class)
+                ->setArgument('$clientId', $config['graph_api']['client'])
+                ->setArgument('$tenantId', $config['graph_api']['tenant'])
+                ->setArgument('$clientSecret', $config['graph_api']['client_secret'])
+            ;
+        } else {
+            $builder->autowire(GraphAPI::class)
+                ->setArgument('$tenantId', '')
+                ->setArgument('$clientId', '')
+                ->setArgument('$clientSecret', '')
+            ;
+        }
     }
 }
